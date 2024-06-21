f23c4ac7d902187a8305724c41346c7789279a75
Yadav-navy <yadav.navy03@gmail.com>
2024-06-21 23:38:47 +0530

    21_June

---
+++ 3.Recursion/15.Merge_Sort.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+
+void PrintArray(int arr[], int start, int end){
+    for (int i = start ; i <= end; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void Merge(int arr[], int start ,int end){
+    int mid = (start + end )/2;
+
+    int len1 = mid - start + 1;
+    int len2 = end - mid ;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    // copying in the new array formed
+    int k = start;
+    // cout << "start "<<start << " "<< end<< endl;
+    for (int i = 0 ; i < len1; i++){
+        first[i] = arr[k];
+        k++;
+    }
+    k = mid + 1;
+    for (int i = 0 ; i < len2; i++){
+        second[i] = arr[k];
+        k++;
+    }
+    // cout << "printing two subarrays that are copied" << endl;
+    // PrintArray(first , 0 , len1-1);
+    // PrintArray(second, 0 , len2-1);
+    // cout << endl;
+
+    int a = 0;
+    int b = 0; // pointers for comparing two arrays
+    k = start;
+    while ( a < len1 && b < len2){
+        if(first[a] >= second[b]){
+            arr[k++] = second[b];
+            b++;
+        }
+        else {
+            arr[k++] = first[a];
+            a++;
+        }
+    }
+    while (a < len1){
+        arr[k++] = first[a++];
+    }
+    
+    while (b < len2){
+        arr[k++] = second[b++];
+    }
+}
+
+void MergeSort(int arr[], int start , int end){
+    // PrintArray(arr, start ,end);
+    if (start >= end){  //base case
+        // cout << "base condition reached" << endl;
+        return; 
+    }
+    int mid = (start + end) / 2;
+    
+
+    MergeSort(arr, start, mid);
+    MergeSort(arr, mid + 1, end);
+    // cout << endl;
+    // cout << "Now Executing Merging" << endl;
+
+    Merge(arr,start,end);
+    // PrintArray(arr, start ,end);
+
+
+
+
+}
+int main(){
+    int arr[6] = {6,75,2,2,2,1};
+    int size = 6;
+    int start = 0 ;
+    int end = size - 1;
+    PrintArray(arr, start, end );
+    MergeSort(arr, start, end);
+    PrintArray(arr, start, end);
+}
+
---
+++ 3.Recursion/16.Quick_Sort.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+
+void PrintArray(int arr[], int start, int end){
+    for (int i = start ; i <= end; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int Partision(int arr[], int start ,int end){
+    int pivot = arr[start];
+    // cout << "Pivot is "<< pivot << endl;
+    int position = 0;
+    for(int i = start + 1 ;i <= end; i++){
+        if (pivot >= arr[i]){
+            position++;
+        }
+    }
+    // cout << "Position is "<< position << endl;
+    int correct_position = start + position;
+    // swap
+    swap(arr[start],arr[correct_position]);
+
+    // Now for moving smaller elements to left of pivot and larger element to right of pivot
+    int a = 0;
+    int b = end;
+
+    while( a < correct_position && b > correct_position){
+        if (arr[a] <= pivot){
+            a++;
+        }
+        if (arr[b] > pivot){
+            b--;
+        }
+        if(arr[a] > pivot && arr[b] < pivot){
+            swap(arr[a],arr[b]);
+            a++ ;
+            b--;
+        }
+    }
+    // cout << "Inside Partision " << endl;
+    // PrintArray(arr, start ,end);
+    // cout << endl;
+    return correct_position;
+
+}
+
+
+
+void QuickSort(int arr[], int start ,int end){
+    // PrintArray(arr, start ,end);
+    // base case
+    if (start >= end){
+        return;
+    }
+
+    int p = Partision(arr, start ,end);
+    QuickSort(arr, start, p - 1);
+    QuickSort(arr, p+1 , end);
+
+
+}
+
+int main(){
+    int arr[6] = {6,2,2,2,8,1};
+    int size = 6;
+    int start = 0 ;
+    int end = size - 1;
+    PrintArray(arr, start, end );
+    QuickSort(arr, start, end);
+    PrintArray(arr, start, end);
+}
