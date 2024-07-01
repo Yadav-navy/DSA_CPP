8debcfc6c872794e61d862e472ee815d0f14267e
Yadav-navy <yadav.navy03@gmail.com>
2024-07-01 23:13:54 +0530

    1_July

---
+++ 4.OOPs/8.Shallow_and_deep_copy.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Hero{
+    char level;
+    int health;
+
+    public:
+    char *name;
+    // Constructor
+    Hero(int health) {
+        cout << this << endl;
+        this -> health = health;
+    }
+
+    Hero(int health, char level) {
+        this -> health = health;
+        this -> level = level;
+    }
+
+    // Copy Constructor
+    Hero(Hero& temp){
+        cout << "Copy Constructor Called"<< endl;
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+    
+    // getter
+    int gethealth(){
+        return health;
+    }
+    void sethealth(int n){
+        health = n;
+    }
+
+    void print(){
+        cout << health << endl;
+        cout << level << endl;
+    }
+
+
+
+};
+
+int main(){
+    Hero a(10,'B');
+    Hero b(a); 
+
+    a.print();
+    b.print();
+
+
+
+}
